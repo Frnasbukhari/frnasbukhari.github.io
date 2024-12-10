8864072174dee86f6ec2c3a1a27fdaacc82387d6
Frnasbukhari <64270666+Frnasbukhari@users.noreply.github.com>
2024-12-10 01:06:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function toggleMenu() {
   const navLinks = document.querySelector('.nav-links');
+  const navToggle = document.querySelector('.nav-toggle');
+  const expanded = navToggle.getAttribute('aria-expanded') === 'true' || false;
+
   if (navLinks.style.display === 'flex') {
     navLinks.style.display = 'none';
   } else {
     navLinks.style.display = 'flex';
   }
+
+  navToggle.setAttribute('aria-expanded', !expanded);
 }
 
 // Update the year in the footer automatically
